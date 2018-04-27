226d177d4c7b51955af62123ed1f551fa8f58bdb
Eric Bastos Gorgens <e.gorgens@gmail.com>
2018-04-27 12:06:47 -0300

    update
    gerar nome da alternativa

--- gerador.r
+++ gerador.r
@@ -1,4 +1,5 @@
 gerador = function(
+  talhao = NA,
   area = NA,
   idadeRot = NA,
   rotacao = NA,
@@ -42,11 +43,11 @@ gerador = function(
       i = i + 1
       #print(producao)
     }
-    return(list(coeficiente, producao))
+    return(list(paste(talhao, "_", icR1, icR2, sep=""), coeficiente, producao))
 }
 
 # Verificar função
 
-print(gerador(area = 19.44, idade = 6, rotacao = 2, icR1 = 7, icR2 = 7, modeloR1 = c(10.40, -0.25, 2.81), modeloR2 = c(14, -0.24, 2.44)))
-print(gerador(area = 19.44, idade = 6, rotacao = 2, icR1 = 6, icR2 = 7, modeloR1 = c(10.40, -0.25, 2.81), modeloR2 = c(14, -0.24, 2.44)))
-print(gerador(area = 15.03, idade = 5, rotacao = 1, icR1 = 8, icR2 = 7, modeloR1 = c(16, -0.18, 2.20), modeloR2 = c(15.13, -0.20, 2.16)))
+print(gerador(talhao = 101, area = 19.44, idade = 6, rotacao = 2, icR1 = 7, icR2 = 7, modeloR1 = c(10.40, -0.25, 2.81), modeloR2 = c(14, -0.24, 2.44)))
+print(gerador(talhao = 101, area = 19.44, idade = 6, rotacao = 2, icR1 = 6, icR2 = 7, modeloR1 = c(10.40, -0.25, 2.81), modeloR2 = c(14, -0.24, 2.44)))
+print(gerador(talhao = 401, area = 15.03, idade = 5, rotacao = 1, icR1 = 8, icR2 = 7, modeloR1 = c(16, -0.18, 2.20), modeloR2 = c(15.13, -0.20, 2.16)))
