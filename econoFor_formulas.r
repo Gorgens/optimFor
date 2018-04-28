974c1c0797ac3f95a5d2d384e652ed68f7aa78a4
Eric Bastos Gorgens <e.gorgens@gmail.com>
2018-04-28 18:26:02 -0300

    Upload fórmulas economicas
    Implementação do VET e do VPL.

---
+++ econoFor_formulas.r
@@ -0,0 +1,36 @@
+vpl = function(
+  receita = NA,
+  custo = NA,
+  i = 0.10
+){
+  #length(receita) == length(custo) # Conferência
+  horizonte = seq(0, length(receita) - 1, 1)
+  
+  VPR = sum(receita / (1 + i)^horizonte)
+  VPC = sum(custo / (1 + i)^horizonte)
+
+  return(VPR - VPC)
+}
+
+# Conferência
+# print(vpl(c(0, 0, 0, 0, 0, 0, 0, 0, 80, 80, 80, 80, 960), 
+#           c(325, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))) # Resutlado esperado: 111.02
+
+vet = function(
+  receita = NA,
+  custo = NA,
+  i = 0.10
+){
+  #length(receita) == length(custo) # Conferência
+  horizonte = seq(length(receita) - 1, 0, -1)
+  
+  VFR = sum(receita * (1 + i)^horizonte)
+  VFC = sum(custo * (1 + i)^horizonte)
+  
+  
+  return((VFR - VFC)/((1+i)^(length(horizonte)-1)-1))
+}
+
+# Conferência
+# print(vet(c(0, 0, 0, 0, 0, 0, 0, 0, 80, 80, 80, 80, 960), 
+#           c(325, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))) # Resutlado esperado: 111.02
