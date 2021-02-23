984125b570c23bea09df535a6cc0db55d504a3e8
Eric Bastos Gorgens <2597395+Gorgens@users.noreply.github.com>
2021-02-23 14:44:08 -0300

    include exploration plots

--- simuladorPlantada/itc_i1i2.R
+++ simuladorPlantada/itc_i1i2.R
@@ -4,56 +4,129 @@ require(plotly)
 
 source('parearDados.R')
 
-# Relação DAP2 = f(DAP1)|sitio
-fig = plot_ly(data = d.baixo, x = ~DAP1)
+## Investigar relações i1/i2 --------------------
 
-d.baixo = dfInv2 %>% filter(sitio == 'baixo')
-m.baixo = lm(DAP2~DAP1, data = d.baixo)
-fig = add_markers(fig, y = ~DAP2, 
-              type="scatter", mode="markers", name = 'baixo', 
-              alpha = 0.2, color = I('red'))
-fig = add_lines(fig, x = ~DAP1, y = fitted(m.baixo), name = 'baixo')
-
-d.medio = dfInv2 %>% filter(sitio == 'medio')
-m.medio = lm(DAP2~DAP1, data = d.medio)
-fig = add_markers(fig, data = d.medio, x = ~DAP1, y = ~DAP2, 
-              type="scatter", mode="markers", name = 'baixo',
-              alpha = 0.2, color = I('green'))
-fig = add_lines(fig, data = d.medio, x = ~DAP1, y = fitted(m.medio), 
-                name = 'baixo', color = I('green') )
-
-d.alto = dfInv2 %>% filter(sitio == 'alto')
-m.alto = lm(DAP2~DAP1, data = d.alto)
-fig = add_markers(fig, data = d.alto, x = ~DAP1, y = ~DAP2, 
-                  type="scatter", mode="markers", name = 'alto', 
-                  alpha = 0.2, color = I('blue'))
-fig = add_lines(fig, data = d.alto, x = ~DAP1, y = fitted(m.alto), 
-                name = 'alto', color = I('blue') )
+## DAP --------------------------------
+# DAP2 = f(DAP1)|sitio
+fig = plot_ly(data = dfinv2, x = ~DAP1, y = ~DAP2, 
+              type="scatter", mode="markers",
+              alpha = 0.7, color = ~sitio)
 fig
 
-
-## Modelo DAP2 = f(DAP1) ---------------------------------------
-fig = plot_ly(data = dfInv2, x = ~DAP1)
-fig = add_markers(fig, y = ~DAP2, 
-                  type="scatter", mode="markers", name = 'DAP', 
-                  alpha = 0.5, color = I('black'))
-m.dap2 = lm(DAP2~DAP1, data = dfInv2)
-fig = add_lines(fig, x = ~DAP1, y = fitted(m.dap2), name = 'DAP')
+# DAP2 = f(DAP1)|cc
+fig = plot_ly(data = dfinv2, x = ~DAP1, y = ~DAP2, 
+              type="scatter", mode="markers",
+              alpha = 0.7, color = ~CC1)
 fig
 
-fig = plot_ly(x = m.dap2$residuals, type = "histogram")
-fig = layout(fig, xaxis = list(range = c(-4, 4)))
+# DAP2 = f(DAP1)|clonesemente
+fig = plot_ly(data = dfinv2, x = ~DAP1, y = ~DAP2, 
+              type="scatter", mode="markers",
+              alpha = 0.7, color = ~CLONESEMENTE1)
 fig
 
-## Modelo ALT2 = f(ALT1) ---------------------------------------
-inv2alt = dfInv2 %>% filter(ALT1 > 0 & ALT2 > 0)
+# DAP2 = f(DAP1)|idade
+fig = plot_ly(data = dfinv2, x = ~DAP1, y = ~DAP2, 
+              type="scatter", mode="markers",
+              alpha = 0.7, color = ~I1)
+fig
+
+# DAP2 = f(DAP1)|desbasteseq
+fig = plot_ly(data = dfinv2, x = ~DAP1, y = ~DAP2, 
+              type="scatter", mode="markers",
+              alpha = 0.7, color = ~DESBASTESEQ)
+fig
+
+# DAP2 = f(DAP1)|tipodesbaste
+fig = plot_ly(data = dfinv2, x = ~DAP1, y = ~DAP2, 
+              type="scatter", mode="markers",
+              alpha = 0.7, color = ~TIPODESBASTE)
+fig
+
+# Teste de Hipótese: DAP2 = f(DAP1)|variables
+lm.dall = lm(DAP2 ~ DAP1, data = dfinv2)
+lm.dsitio = lm(DAP2 ~ DAP1:sitio, data = dfinv2)
+anova(lm.dall, lm.dsitio)
+lm.dcc = lm(DAP2 ~ DAP1:CC1, data = dfinv2)
+anova(lm.dall, lm.dcc)
+lm.dcs = lm(DAP2 ~ DAP1:CLONESEMENTE1, data = dfinv2)
+anova(lm.dall, lm.dcs)
+lm.didade = lm(DAP2 ~ DAP1 * I1, data = dfinv2)         
+anova(lm.didade, lm.dall)                               
+lm.dall = lm(DAP2 ~ DAP1, data = dfinv2[!is.na(dfinv2$DESBASTESEQ),])
+lm.ddes = lm(DAP2 ~ DAP1:factor(DESBASTESEQ), data = dfinv2[!is.na(dfinv2$DESBASTESEQ),])
+anova(lm.dall, lm.ddes)                                 
+
+lm.dred = lm(DAP2 ~ -1 + DAP1, data = dfinv2[!is.na(dfinv2$DESBASTESEQ),])
+lm.dcompleto = lm(DAP2 ~ -1 + DAP1 + sitio + I1 + CLONESEMENTE1 + factor(DESBASTESEQ), 
+                  data = dfinv2[!is.na(dfinv2$DESBASTESEQ),])
+anova(lm.dred, lm.dcompleto)
+
+## Altura --------------------------------
+inv2alt = dfinv2 %>% filter(ALT1 > 0 & ALT2 > 0)
+
+# ALT2 = f(ALT1)|sitio
 fig = plot_ly(data = inv2alt, x = ~ALT1, y = ~ALT2, 
-                  type="scatter", mode="markers", name = 'ALT', 
-                  alpha = 0.3, color = I('black'))
-m.alt2 = lm(ALT2~ALT1, data = inv2alt)
-fig = add_lines(fig, x = ~ALT1, y = fitted(m.alt2), name = 'ALT')
+              type="scatter", mode="markers",
+              alpha = 0.7, color = ~sitio)
 fig
 
-fig = plot_ly(x = m.alt2$residuals, type = "histogram")
-fig = layout(fig, xaxis = list(range = c(-4, 4)))
+# ALT2 = f(ALT1)|cc
+fig = plot_ly(data = inv2alt, x = ~ALT1, y = ~ALT2, 
+              type="scatter", mode="markers",
+              alpha = 0.7, color = ~CC1)
 fig
+
+# ALT2 = f(ALT1)|clonesemente
+fig = plot_ly(data = inv2alt, x = ~ALT1, y = ~ALT2, 
+              type="scatter", mode="markers",
+              alpha = 0.7, color = ~CLONESEMENTE1)
+fig
+
+# ALT2 = f(ALT1)|idade
+fig = plot_ly(data = inv2alt, x = ~ALT1, y = ~ALT2, 
+              type="scatter", mode="markers",
+              alpha = 0.7, color = ~I1)
+fig
+
+# ALT2 = f(ALT1)|desbasteseq
+fig = plot_ly(data = inv2alt, x = ~ALT1, y = ~ALT2, 
+              type="scatter", mode="markers",
+              alpha = 0.7, color = ~DESBASTESEQ)
+fig
+
+# ALT2 = f(ALT1)|tipodesbaste
+fig = plot_ly(data = inv2alt, x = ~ALT1, y = ~ALT2, 
+              type="scatter", mode="markers",
+              alpha = 0.7, color = ~TIPODESBASTE)
+fig
+
+# ALT2 = f(ALT1)|DAP1
+fig = plot_ly(data = inv2alt, x = ~ALT1, y = ~ALT2, 
+              type="scatter", mode="markers",
+              alpha = 0.7, color = ~log(DAP1))
+fig
+
+# Teste de Hipótese: ALT2 = f(ALT1)|variables
+lm.aall = lm(ALT2 ~ ALT1, data = inv2alt)
+lm.asitio = lm(ALT2 ~ ALT1:sitio, data = inv2alt)
+anova(lm.aall, lm.asitio)
+lm.acc = lm(ALT2 ~ ALT1:CC1, data = inv2alt)
+anova(lm.aall, lm.acc)
+lm.acs = lm(ALT2 ~ ALT1:CLONESEMENTE1, data = inv2alt)
+anova(lm.aall, lm.acs)
+lm.aidade = lm(ALT2 ~ ALT1 * I1, data = inv2alt)         
+anova(lm.aidade, lm.aall) 
+lm.adap = lm(ALT2 ~ ALT1 * DAP1, data = inv2alt)         
+anova(lm.adap, lm.aall) 
+lm.aall = lm(ALT2 ~ ALT1, data = inv2alt[!is.na(inv2alt$DESBASTESEQ),])
+lm.ades = lm(ALT2 ~ ALT1:factor(DESBASTESEQ), data = inv2alt[!is.na(inv2alt$DESBASTESEQ),])
+anova(lm.aall, lm.ades)                                 
+
+lm.ared = lm(ALT2 ~ -1 + ALT1, data = inv2alt[!is.na(inv2alt$DESBASTESEQ),])
+lm.acompleto = lm(ALT2 ~ -1 + ALT1  + DAP1 + sitio + I1 + CLONESEMENTE1 + factor(DESBASTESEQ), 
+                  data = inv2alt[!is.na(inv2alt$DESBASTESEQ),])
+anova(lm.ared, lm.acompleto)
+summary(lm.acompleto)
+
+
