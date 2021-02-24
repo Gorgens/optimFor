0123e80823838f36fdf988bed97552ffaeb1cfba
Eric Bastos Gorgens <2597395+Gorgens@users.noreply.github.com>
2021-02-24 17:52:00 -0300

    Add files via upload

--- simuladorPlantada/itc_disInc.R
+++ simuladorPlantada/itc_disInc.R
@@ -1,4 +1,64 @@
 require(ggplot2)
 require(dplyr)
+require(plotly)
+require(fitdistrplus)
 
 source('parearDados.R')
+
+## Investigar relações do incremento --------------------
+
+# IncAnual = f(DAP1)|sitio
+fig = plot_ly(data = dfinv2, x = ~DAP1, y = ~incDapAnual, 
+              type="scatter", mode="markers",
+              alpha = 0.5, color = ~sitio)
+fig
+
+# IncAnualPerc = f(DAP1)|sitio
+fig = plot_ly(data = dfinv2, x = ~DAP1, y = ~incDapAnualPerc, 
+              type="scatter", mode="markers",
+              alpha = 0.5, color = ~sitio)
+fig
+
+# IncAnualPerc | sitio
+fig = plot_ly(data = dfinv2, x = ~incDapAnualPerc, 
+              type="histogram", alpha = 0.5, color = ~sitio, nbinsx = 15)
+fig = fig %>% layout(barmode = "overlay")
+fig
+
+# IncAnualPerc | DESBASTESEQ
+fig = plot_ly(data = dfinv2, x = ~incDapAnualPerc, 
+              type="histogram", alpha = 0.5, color = ~factor(DESBASTESEQ), nbinsx = 15)
+fig = fig %>% layout(barmode = "overlay")
+fig
+
+# IncAnualPerc | CLONESEMENTE
+fig = plot_ly(data = dfinv2, x = ~incDapAnualPerc, 
+              type="histogram", alpha = 0.5, color = ~factor(CLONESEMENTE1), nbinsx = 15)
+fig = fig %>% layout(barmode = "overlay")
+fig
+
+# Modelo gamma para incDapAnualPerc = f(DAP1 + I1 + CLONESEMENTE + sitio + DESBASTESEQ)
+dfinv2 = dfinv2 %>%
+  filter(incDapAnualPerc > 0)
+gammaIncDapAnualPerc = glm(data = dfinv2, incDapAnualPerc ~ DAP1 + I1 + CLONESEMENTE1 + sitio + DESBASTESEQ, family=Gamma)
+summary(gammaIncDapAnualPerc)
+
+# Distribuicao do incremento = f(CC1 + CI1 + CLONESEMENTE1 + sitio + factor(DESBASTESEQ))
+parametros = list(15, 2.5, 'SEED', 'alto', 0)
+fig = plot_ly(alpha = 0.6, nbinsx = 15)
+temp = subset(dfinv2, CC1 = parametros[[1]] & CI1 == parametros[[2]], CLONESEMENTE1 == parametros[[3]] & sitio ==parametros[[4]] & DESBASTESEQ == parametros[[5]])
+fig = fig %>% add_histogram(x = temp$incDapAnualPerc, name = paste(parametros, collapse = '-'))
+temp = subset(dfinv2, CC1 = parametros[[1]] & CI1 == parametros[[2]], CLONESEMENTE1 == parametros[[3]] & sitio ==parametros[[4]] & DESBASTESEQ == parametros[[5]])
+fig = fig %>% add_histogram(x = temp$incDapAnualPerc, name = paste(parametros, collapse = '-'))
+temp = subset(dfinv2, CC1 = parametros[[1]] & CI1 == parametros[[2]], CLONESEMENTE1 == parametros[[3]] & sitio ==parametros[[4]] & DESBASTESEQ == parametros[[5]])
+fig = fig %>% add_histogram(x = temp$incDapAnualPerc, name = paste(parametros, collapse = '-'))
+temp = subset(dfinv2, CC1 = parametros[[1]] & CI1 == parametros[[2]], CLONESEMENTE1 == parametros[[3]] & sitio ==parametros[[4]] & DESBASTESEQ == parametros[[5]])
+fig = fig %>% add_histogram(x = temp$incDapAnualPerc, name = paste(parametros, collapse = '-'))
+fig = fig %>% layout(barmode = "overlay")
+fig
+
+# Ajustar parâmetros distribuição
+temp = subset(dfinv2, CLONESEMENTE1 == 'SEED' & sitio =='alto' & DESBASTESEQ == 0)
+fit.gamma <- fitdist(temp$incDapAnualPerc, distr = "gamma", method = "mle")
+coef(fit.gamma)
+rgamma(1, coef(fit.gamma)[1], coef(fit.gamma)[2])
--- simuladorPlantada/parearDados.R
+++ simuladorPlantada/parearDados.R
@@ -6,24 +6,32 @@ anoDesbaste = read.csv('desbaste.csv')
 
 # Organiza as bases e une
 df2018 = df2018 %>% select(-FILA1, -FILA2)
-dfInv = df2017 %>% bind_rows(df2018)
+dfinv = df2017 %>% bind_rows(df2018)
 
-dfInv$sitio = cut(dfInv$S, 3, labels = c('baixo', 'medio', 'alto'))
-dfInv$idadeInteira = floor(dfInv$I1)
-dfInv$intervaloMed = dfInv$I2 - dfInv$I1
-dfInv$incrementoDap = dfInv$DAP2 - dfInv$DAP1
-dfInv$incDapAnual = dfInv$incrementoDap / dfInv$intervaloMed
-dfInv$incDapAnualPerc = dfInv$incDapAnual / dfInv$DAP1
-dfInv = dfInv %>%  
+dfinv$sitio = cut(dfinv$S, 3, labels = c('baixo', 'medio', 'alto'))
+dfinv$idadeInteira = floor(dfinv$I1)
+dfinv$intervaloMed = dfinv$I2 - dfinv$I1
+dfinv$incrementoDap = dfinv$DAP2 - dfinv$DAP1
+dfinv$incDapAnual = dfinv$incrementoDap / dfinv$intervaloMed
+dfinv$incDapAnualPerc = dfinv$incDapAnual / dfinv$DAP1
+dfinv = dfinv %>%  
   mutate(binarioCresce = if_else(incDapAnual==0,0,1))
 
-dfInv2 = dfInv %>% left_join(anoDesbaste, by = c('CODPROJETO', 'CODTALHAO', 'ANOREF1'))
+dfinv2 = dfinv %>% left_join(anoDesbaste, by = c('CODPROJETO', 'CODTALHAO', 'ANOREF1'))
 
 
 intervaloClasse = 10.0
 diametroMin = 10.0
-classeMax = ((max(dfInv2$DAP1) %/% intervaloClasse) + 1) * intervaloClasse
+classeMax = ((max(dfinv2$DAP1) %/% intervaloClasse) + 1) * intervaloClasse
 intervalos = seq(diametroMin, classeMax, intervaloClasse) - 0.001
 centros = head(round(intervalos + intervaloClasse/2.0, 0), -1)
-dfInv2 = dfInv2[dfInv2['DAP1'] >= diametroMin,]
-dfInv2$CC1 = cut(dfInv2$DAP1, breaks = intervalos, labels=centros)
+dfinv2 = dfinv2[dfinv2['DAP1'] >= diametroMin,]
+dfinv2$CC1 = cut(dfinv2$DAP1, breaks = intervalos, labels=centros)
+
+intervaloClasse = 5.0
+idadeMin = 0
+idadeMax = ((max(dfinv2$I1) %/% intervaloClasse) + 1) * intervaloClasse
+intervalos = seq(idadeMin, idadeMax, intervaloClasse) - 0.001
+centros = head(round(intervalos + intervaloClasse/2.0, 1), -1)
+dfinv2 = dfinv2[dfinv2['I1'] >= idadeMin,]
+dfinv2$CI1 = cut(dfinv2$I1, breaks = intervalos, labels=centros)
