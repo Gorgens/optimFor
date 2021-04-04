eac066ed1c4ad8a32d9d235dbfcc0df50ee50b4a
Eric Bastos Gorgens <2597395+Gorgens@users.noreply.github.com>
2021-04-04 17:21:27 -0300

    explore ecological group criterias

--- loggingCriteria/15_exploringPaisagensMerged.R
+++ loggingCriteria/15_exploringPaisagensMerged.R
@@ -4,6 +4,28 @@ require(plotly)
 require(magrittr)
 
 inv.paisagens = read.csv("10_invPaisagensMerged.csv")
+inv.paisagens.filtered = inv.paisagens %>%
+  filter(type == "O" | is.na(type)) %>%                                         # remover não árvores
+  filter(scientific.name != 'NI') %>%                                           # remover indivíduos não indentificados
+  drop_na(DBH)   
+
+
+ggplot(inv.paisagens.filtered, aes(DBH, Htot)) +                                # gráfico da hipsometria dos dados do paper
+  geom_point(alpha = 0.1) + 
+  theme_bw() + theme(panel.grid.major = element_blank(),
+                     panel.grid.minor = element_blank(),
+                     panel.background = element_blank(),
+                     axis.line = element_line(colour = "black"))
+ggsave("./plot/graphHipsometria.png")
+
+parcelasArea = inv.paisagens.filtered %>%                                       # conta número de parcelas em cada área
+  group_by(area, plot, subplot, year) %>%
+  summarise(obs = n()) %>%
+  group_by(area, year) %>%
+  summarise(nplots = n(), trees = sum(obs)) %>%
+  group_by(area) %>%
+  summarise(nplots = mean(nplots), trees = sum(trees))
+write.csv(parcelasArea, './tables/parcelaPorArea.csv')
 
 #### G1 - Scatterplot ---------------
 fig = inv.paisagens %>% plot_ly(x = ~DBH, y = ~Hcom, color = ~area)
--- loggingCriteria/21_criteriaEcologicalGroup.R
+++ loggingCriteria/21_criteriaEcologicalGroup.R
@@ -13,24 +13,26 @@ inv.paisagens.filtered = inv.paisagens %>%
   filter(scientific.name != 'NI') %>%                                           # remover indivíduos não indentificados
   drop_na(DBH)                                                                  # remover valores NA no DBH
 
-ggplot(inv.paisagens.filtered, aes(DBH, Htot)) +                                # gráfico da hipsometria dos dados do paper
-  geom_point(alpha = 0.1) + 
-  theme_bw() + theme(panel.grid.major = element_blank(),
-                     panel.grid.minor = element_blank(),
-                     panel.background = element_blank(),
-                     axis.line = element_line(colour = "black"))
-ggsave("./plot/graphHipsometria.png")
-
 parcelasArea = inv.paisagens.filtered %>%                                       # conta número de parcelas em cada área
   group_by(area, plot, subplot, year) %>%
   summarise(obs = n()) %>%
   group_by(area, year) %>%
-  summarise(nplots = n()) %>%
+  summarise(nplots = n(), trees = sum(obs)) %>%
   group_by(area) %>%
-  summarise(nplots = mean(nplots))
+  summarise(nplots = mean(nplots), trees = sum(trees))
 
-filogenia = inv.paisagens.filtered %>%                                          # filogenia das spécies estudadas
-  filter(comercial == 1) %>%
+grupo = "Intermediate"
+grupo = "Shade-tolerant"
+grupo = "Light-demanding" 
+grupo = "Pioneer"
+grupo = "Emergent"
+
+## Grupo Ecológico -------------------------------------------------------------
+grupoEcologico = inv.paisagens.filtered %>%
+  filter(GrupoEco == grupo)
+
+# Filogenia
+filogenia = grupoEcologico %>%                                                    # filogenia das spécies estudadas
   group_by(scientific.name, genera.name, family.name) %>%
   summarise(n = n()) %>%
   drop_na(family.name) %>%
@@ -38,40 +40,32 @@ filogenia = inv.paisagens.filtered %>%                                          
   filter(family.name != 'NI')
 
 phy = phylo.maker(filogenia[,1:3])
-#write.tree(phy$scenario.3, "Figure.1.tre")
 tree = phy$scenario.3
-png('./plot/filo_Comercial.png', width = 10, height = 10, units = 'cm', res = 300)
-plotTree(tree, type='fan', fsize=0.5, lwd=3, ftype='i')
+png(paste0('./plot/filo_', grupo, '.png'), width = 10, height = 10, units = 'cm', res = 300)
+plotTree(tree, type='fan', fsize=0.5, lwd=1, ftype='i')
 dev.off()
 
-### Critérios por espécies comerciais  -------------------------------
-speciesList = inv.paisagens.filtered %>%                                        # Lista das espécies com maior número de árvores
-  filter(comercial == 1) %>%                                                    # filtra espécies comerciais
-  group_by(scientific.name) %>%
-  summarise(ntree = n()) %>%
-  filter(ntree > 60) %>%
-  select(scientific.name)
+### Critérios por espécies grupo escológico ------------------------------------
 
 # Densidade de indivíduos
 arvHaEspecie = inv.paisagens.filtered %>%
   filter(DBH >= 10) %>%
-  group_by(area, plot, subplot, year, scientific.name) %>% 
+  group_by(area, plot, subplot, year, GrupoEco) %>% 
   summarise(ntree = sum(eqTree)) %>%
   drop_na(ntree) %>%
-  group_by(area, plot, subplot, scientific.name) %>%
+  group_by(area, plot, subplot, GrupoEco) %>%
   summarise(ntree = mean(ntree)) %>%
-  group_by(area, scientific.name) %>%
+  group_by(area, GrupoEco) %>%
   summarise(ntree = sum(ntree)) %>%
   left_join(parcelasArea) %>%
   mutate(arvha = ntree / nplots) %>%
-  group_by(scientific.name) %>%
-  summarise(arvha = mean(arvha)) %>%
-  filter(scientific.name %in% speciesList$scientific.name)
+  group_by(GrupoEco) %>%
+  summarise(arvha = mean(arvha))
 
 # Distribuição diamétrica
-for(i in speciesList$scientific.name){
+for(i in unique(inv.paisagens.filtered$GrupoEco)){
   numeroArvores = inv.paisagens.filtered %>%
-    filter(scientific.name == i) %>%
+    filter(GrupoEco == i) %>%
     filter(DBH >= 10) %>%
     group_by(area, plot, subplot, year, cc) %>% 
     summarise(ntree = sum(eqTree))  %>%
@@ -82,7 +76,7 @@ for(i in speciesList$scientific.name){
     mutate(arvha = narv / nplots) %>%
     group_by(cc) %>%
     summarise(arvha = mean(arvha))
-    
+  
   ggplot(numeroArvores, aes(cc, arvha)) + geom_col() +
     xlab('Diameter distribution') + ylab('Trees per hectare') +
     ggtitle(paste0(i)) + 
@@ -90,29 +84,20 @@ for(i in speciesList$scientific.name){
                        panel.grid.minor = element_blank(),
                        panel.background = element_blank(),
                        axis.line = element_line(colour = "black"))
-  ggsave(paste0('./plot/dd_',i, '.png'))
+  ggsave(paste0('./plot/dd_',i, '.png'), width = 13, height = 8, units = 'cm')
 }
 
 # Ciclo de exploração
 #pareado = dcast(inv.paisagens, area + plot + subplot + tree ~ year, value.var='DBH', fun.aggregate=sum)
 incremento = inv.paisagens %>%
-  group_by(area, plot, subplot, tree, scientific.name) %>%
+  group_by(area, plot, subplot, tree, scientific.name, GrupoEco) %>%
   summarise(cc = min(cc), minDBH = min(DBH), maxDBH = max(DBH), 
             inc = max(DBH) - min(DBH), intervMed = max(year) - min(year), 
             incAnual = inc / intervMed) %>%
   filter(inc > 0)
 
-ggplot(incremento, aes(as.factor(cc), incAnual)) + geom_boxplot() +
-  xlab('Diameter class') +
-  theme_bw() + theme(panel.grid.major = element_blank(),
-                     panel.grid.minor = element_blank(),
-                     panel.background = element_blank(),
-                     axis.line = element_line(colour = "black"))
-ggsave('./plot/incrementoAnual.png')
-
-incrementoSpecies = incremento %>%
-  filter(scientific.name %in% speciesList$scientific.name) %>%
-  group_by(scientific.name) %>%
+incrementoGrupo = incremento %>%
+  group_by(GrupoEco) %>%
   summarise(incAnual = mean(incAnual), tp = 50 / incAnual)
 
 # Padrão espacial
